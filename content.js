cf6a04546e387e2622ef43f325a57d3e6edcf423
alberthashani <albert.hashani@gmail.com>
2024-11-16 19:27:26 +0100

    refactor: use switch instead of ifs for readability

--- content.js
+++ content.js
@@ -5,42 +5,46 @@ var originalVolume = null; // Store the original volume
 chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
   var videoPlayer = document.querySelector('video');
 
-  if (request.action === 'getVolume') {
-    // Send the current volume back to the popup
-    console.log('Sending current volume:', videoPlayer ? videoPlayer.volume : 0);
-    sendResponse({ volume: videoPlayer ? videoPlayer.volume : 0 });
-  } else if (request.action === 'setVolume' && videoPlayer && extensionEnabled) {
-    console.log('Setting volume:', request.volume);
-    // Set the new volume
-    videoPlayer.volume = request.volume;
-  } else if (request.action === 'enableExtension') {
-    console.log('Extension enabled');
-    if (videoPlayer) {
-      // Store the current video volume before enabling
-      originalVolume = videoPlayer.volume;
-      // Apply the slider volume
-      chrome.runtime.sendMessage({ action: 'getSliderVolume' }, function(response) {
-        if (response && response.sliderVolume !== undefined) {
-          videoPlayer.volume = response.sliderVolume;
-        }
-      });
-    }
-    // Enable the extension
-    extensionEnabled = true;
-    console.log('Extension enabled, original volume:', originalVolume);
-  } else if (request.action === 'disableExtension') {
-    console.log('Extension disabled');
-    if (videoPlayer && originalVolume !== null) {
-      // Restore the original volume
-      videoPlayer.volume = originalVolume;
-      originalVolume = null;
-    }
-    // Disable the extension
-    extensionEnabled = false;
-    console.log('Extension disabled, restored volume');
-  } else if (request.action === 'getExtensionState') {
-    console.log('Sending extension state:', extensionEnabled);
-    // Send the current extension state back to the popup
-    sendResponse({ isEnabled: extensionEnabled });
+  switch (request.action) {
+    case 'getVolume':
+      console.log('Sending current volume:', videoPlayer ? videoPlayer.volume : 0);
+      sendResponse({ volume: videoPlayer ? videoPlayer.volume : 0 });
+      break;
+      
+    case 'setVolume':
+      if (videoPlayer && extensionEnabled) {
+        console.log('Setting volume:', request.volume);
+        videoPlayer.volume = request.volume;
+      }
+      break;
+      
+    case 'enableExtension':
+      console.log('Extension enabled');
+      if (videoPlayer) {
+        originalVolume = videoPlayer.volume;
+        chrome.runtime.sendMessage({ action: 'getSliderVolume' }, function(response) {
+          if (response && response.sliderVolume !== undefined) {
+            videoPlayer.volume = response.sliderVolume;
+          }
+        });
+      }
+      extensionEnabled = true;
+      console.log('Extension enabled, original volume:', originalVolume);
+      break;
+      
+    case 'disableExtension':
+      console.log('Extension disabled');
+      if (videoPlayer && originalVolume !== null) {
+        videoPlayer.volume = originalVolume;
+        originalVolume = null;
+      }
+      extensionEnabled = false;
+      console.log('Extension disabled, restored volume');
+      break;
+      
+    case 'getExtensionState':
+      console.log('Sending extension state:', extensionEnabled);
+      sendResponse({ isEnabled: extensionEnabled });
+      break;
   }
 });
